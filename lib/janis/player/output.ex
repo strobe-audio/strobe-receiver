4fa968b0b7e8f98f1fc5d960de98a5409c19d300
Garry Hill <garry@magnetised.net>
2015-09-15 17:28:15 +0100

    Just ignore failures to connect to the audio socket

--- lib/janis/player/output.ex
+++ lib/janis/player/output.ex
@@ -9,12 +9,21 @@ defmodule Janis.Player.Output do
 
   def init({address, port}) do
     Logger.info "Connecting to audio on address #{inspect address}:#{port}"
-    {:ok, socket} = :gen_tcp.connect(address, port, [:inet, :binary, active: true])
+    socket = case :gen_tcp.connect(address, port, [:inet, :binary, active: true]) do
+      {:ok, socket} -> socket
+      {:error, :econnrefused} ->
+        Logger.warn "No audio output..."
+        nil
+    end
     {:ok, socket}
   end
 
   def send(data) do
     GenServer.cast(@name, {:send, data})
+  end
+
+  def handle_cast({:send, data}, socket) when is_nil(socket) do
+    {:noreply, socket}
   end
 
   def handle_cast({:send, data}, socket) do
