37735248e7917707863ae9332f51a870bec089a4
Garry Hill <garry@magnetised.net>
2015-10-21 21:18:33 +0100

    Don't need the emitter code any more

--- lib/janis/player/supervisor.ex
+++ lib/janis/player/supervisor.ex
@@ -6,20 +6,7 @@ defmodule Janis.Player.Supervisor do
   end
 
   def init({address, {_packet_interval, _packet_size} = stream_info}) do
-
-    pool_options = [
-      name: {:local, Janis.Player.EmitterPool},
-      worker_module: Janis.Player.Emitter,
-      size: 6,
-      max_overflow: 2
-    ]
-
     children = [
-      :poolboy.child_spec(Janis.Player.EmitterPool, pool_options, [
-        interval: _packet_interval,
-        packet_size: _packet_size,
-        pool: Janis.Player.EmitterPool
-      ]),
       worker(Janis.Player.Buffer, [stream_info, Janis.Player.Buffer]),
       worker(Janis.Player.Socket, [address, stream_info, Janis.Player.Buffer]),
     ]
