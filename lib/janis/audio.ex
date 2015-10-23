be2ad0feffeaad381e88a9c429cc00c874390e9f
Garry Hill <garry@magnetised.net>
2015-10-23 16:05:10 +0100

    Remove test method

--- lib/janis/audio.ex
+++ lib/janis/audio.ex
@@ -24,17 +24,4 @@ defmodule Janis.Audio do
   def time do
     GenServer.call(@name, :time)
   end
-
-  def test(n \\ 5) do
-    c = Enum.min [50, n]
-    {:ok, data} = File.open "../audio/176400-shubert-piano-quintet.raw", [:read], fn(file) ->
-      IO.binread file, :all
-    end
-    now = monotonic_microseconds
-    Enum.each 0..c, fn(i) ->
-      skip_bytes = 3528*i
-      << _skip::binary-size(skip_bytes), packet::binary-size(3528), data::binary >> = data
-      play({now + (i * 2000), packet})
-    end
-  end
 end
