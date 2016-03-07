84a74a089978b42f4844dcc26d01b358f748a1fd
Garry Hill <garry@magnetised.net>
2016-03-07 11:11:15 +0000

    Require elixir 1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Janis.Mixfile do
   def project do
     [app: :janis,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      compilers: [:make, :elixir, :app],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
