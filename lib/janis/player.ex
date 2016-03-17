0b97fcdb89f09066d13b01451210d2aaed586619
Garry Hill <garry@magnetised.net>
2016-03-17 17:41:19 +0000

    Catch closed connections and attempt a re-connect

--- lib/janis/player.ex
+++ lib/janis/player.ex
@@ -1,25 +1,51 @@
 defmodule Janis.Player do
   use GenServer
 
-  @name Janis.Player
-
   alias Janis.Player.Buffer
   alias Janis.Player.Socket.Data
   alias Janis.Player.Socket.Ctrl
 
+  require Logger
+
   defmodule S do
     @moduledoc false
-    defstruct [:buffer, :data, :ctrl]
+
+    defstruct [
+      :buffer,
+      :data,
+      :ctrl,
+      :broadcaster,
+      :latency,
+    ]
   end
+
+  @name Janis.Player
 
   def start_link(broadcaster, latency) do
     GenServer.start_link(__MODULE__, {broadcaster, latency})
   end
 
   def init({broadcaster, latency}) do
+    Process.flag(:trap_exit, :true)
     {:ok, buffer} = Buffer.start_link(broadcaster)
-    {:ok, data}   = Data.start_link(broadcaster, latency, buffer)
-    {:ok, ctrl}   = Ctrl.start_link(broadcaster, latency, buffer)
-    {:ok, %S{buffer: buffer, data: data, ctrl: ctrl}}
+    {:ok, data}   = start_connection(Data, broadcaster, latency, buffer)
+    {:ok, ctrl}   = start_connection(Ctrl, broadcaster, latency, buffer)
+    {:ok, %S{broadcaster: broadcaster, latency: latency, buffer: buffer, data: data, ctrl: ctrl}}
+  end
+
+  def handle_info({:EXIT, pid, :tcp_closed}, %S{data: pid} = state) do
+    Logger.warn "Data connection closed, re-connecting..."
+    {:ok, data} = start_connection(Data, state.broadcaster, state.latency, state.buffer)
+    {:noreply, %S{state | data: data}}
+  end
+
+  def handle_info({:EXIT, pid, :tcp_closed}, %S{ctrl: pid} = state) do
+    Logger.warn "Ctrl connection closed, re-connecting..."
+    {:ok, data} = start_connection(Ctrl, state.broadcaster, state.latency, state.buffer)
+    {:noreply, %S{state | data: data}}
+  end
+
+  defp start_connection(module, broadcaster, latency, buffer) do
+    module.start_link(broadcaster, latency, buffer)
   end
 end
