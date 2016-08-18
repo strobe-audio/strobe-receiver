4b6a54f588e93ec41cea94d5c9071e9f58f42b1a
Garry Hill <garry@magnetised.net>
2016-08-18 12:29:18 +0100

    Add missing func definition

--- c_src/pid.h
+++ c_src/pid.h
@@ -8,4 +8,5 @@ typedef struct {
 } pid_state_t;
 
 void pid_init(pid_state_t *pid, double kp, double ki, double kd);
+void pid_reset(pid_state_t *pid);
 double pid_control(pid_state_t *pid, double time, double measured_value, double setpoint);
